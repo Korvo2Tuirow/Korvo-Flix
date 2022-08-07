16525743093623dc14f4e6550c26ab3de893cbc9
Korvo2Tuirow <korvo2irow@live.com>
2022-08-07 00:49:39 -0300

    teste
    mais testes

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ let frente = document.getElementById("filmes")
 
 let tras = document.querySelector('#modal');
 
+let header = document.querySelector('#head')
+
 frente.addEventListener('click', function()  {
-tras.style.zIndex="10"
+tras.style.display="block",
+header.style.display="none"
 })
 
 tras.addEventListener('click', function()  {
-    tras.style.zIndex="0"
+    tras.style.display="none"
+    header.style.display="flex"
     })
